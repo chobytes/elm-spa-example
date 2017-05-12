7a9c672143d9271ffbd216cf73b33ef18d3d10d0
Felicity Lovelace <chobytes@icloud.com>
2017-05-12 01:53:25 -0500

    added very basic nav

---
+++ src/View/Nav.elm
@@ -0,0 +1,15 @@
+module View.Nav exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+
+view : Html msg
+view =
+    div []
+        [ ul []
+              [ li [] [ a [ href "#" ] [ text "Home"] ]
+              , li [] [ a [ href "#users" ] [ text "Users"]]
+              , li [] [ a [ href "#nonesenseurl" ] [ text "404" ]]
+              ]
+        ]
